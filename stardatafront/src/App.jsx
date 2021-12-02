7fab6d36b0110f94c920ecacdc5b3c66ce193faf
SergioS11 <sergiosalas172017@hotmail.com>
2021-12-02 15:53:19 -0500

    Actualizción

--- stardatafront/src/App.jsx
+++ stardatafront/src/App.jsx
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+//views
+import Home from "./home/home.view";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+          <Routes>
+             <Route path="/" exact/>
+                <Home />
+             </Route>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
