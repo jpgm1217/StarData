234bdca53314e1901a2d70253cf1df73f7af084a
SergioS11 <sergiosalas172017@hotmail.com>
2021-12-02 15:49:17 -0500

    Actualización del repositorio con la instalación e importacion de Apollo

---
+++ stardatafront/src/home/home.view.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+// assets
+import logo from '.assets/logo-dom.';
+
+const Home = () => {
+  return (
+    <>
+  
+     <p>
+       Edit <code>src/App.js</code> and save to reload.
+     </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+     >
+      Learn React
+     </a>
+    </>
+ );
+};
+
+export default Home;
